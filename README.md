c92838283c042fc3c03ded9e862b8a3b581e80b1
argxkr <anuragkr90@fb.com>
2022-10-08 01:27:09 -0700

    noresqa mos

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # NORESQA: Speech Quality Assessment using Non-Matching References
 
 
-This is a Pytorch implementation for using NORESQA. It contains minimal code to predict speech quality using NORESQA. Please check out our **Neurips 2021** paper referenced below for details.
+This is a Pytorch implementation for using NORESQA. The NORESQA framework uses non-matching
+references (NMT) along with the given test speech signal to estimate speech quality. Under this framework
+we have two metrics:
 
-### Minimal basic usages as Speech Quality Assessment Metric.
+- *NORESQA-score*: A metric based on SI-SDR for speech. The model predicts absolute relative SI-SDR [1] between test and NMR and probability of test cleaner than the NMR
+- *NORESQA-MOS*: NORESQA-MOS is designed to estimate Mean Opinion Score (MOS) [2]. The output of NORESQA-MOS is MOS score.
+
+The detailed of NORESQA framework and *NORESQA-score* is in our **Neurips 2021** paper referenced below.The details of *NORESQA-MOS* is in our **Interspeech 2022** paper.
+
+### Using this library as Speech Quality Assessment Metric.
 
 ##  Setup and basic usage
 
-Required python libraries (latest): Pytorch with GPU support + Scipy + Numpy (>=1.14) + Librosa.
+Required python libraries (latest): Pytorch with GPU support + Scipy + Numpy (>=1.14) + Librosa + fairseq.
+
 Install all dependencies in a conda environment by using:
 ```
 conda env create -f requirements.yml
@@ -16,30 +24,39 @@ Activate the created environment by:
 ```
 conda activate noresqa
 ```
+Set CONFIG_PATH in main.py. This is path of *Wav2Vec 2.0 Base* model used for instantiating the NORESQA-MOS model. For default **download *Wav2Vec 2.0 Base* model from [this link](https://github.com/facebookresearch/fairseq/blob/main/examples/wav2vec/README.md) and put inside models/ directory.**
 
 Additional notes:
-- Warning: Make sure your libraries (Cuda, Cudnn,...) are compatible with the pytorch version you're using.
-- Tested on Nvidia GeForce RTX 2080 GPU with Cuda (>=9.2) and CuDNN (>=7.3.0). CPU mode should also work.
+- Warning: Make sure your libraries (Cuda, Cudnn,...) are compatible with the pytorch version you're using or the code will not run.
+
+- *NORESQA-MOS* predictions are based on assumptions that the provided NMR(s) is(are) clean. Use only clean NMRs to predict MOS.
+
 - The current pretrained models support **sampling rate = 16KHz**. The provided code automatically resamples the recording to 16KHz.
 
 Please run the metric by using:
 ```
 usage:
 
-python main.py --GPU_id -1 --mode file --test_file path1 --nmr path2
+python main.py --GPU_id -1 --metric_type 1 --mode file --test_file path1 --nmr path2
 
 arguments:
 --GPU_id         [-1 or 0,1,2,3,...] specify -1 for CPU, and 0,1,2,3 .. as gpu numbers
+--metric_type    0 --> *NORESQA-score*, 1 --> *NORESQA-MOS*
 --mode           [file,list] using single nmr or a list of nmr
 --test_file      [path1] -> path of the test recording
 --nmr            [path2 of file, or txt file with filenames]
 ```
 
-The default output of the code should look like:
+For *NORESQA-score* the output should look like:
 
 ```
 Probaility of the test speech cleaner than the given NMR = 0.11526459
 NORESQA score of the test speech with respect to the given NMR = 18.595860697038006
+```
+
+For *NORESQA-MOS* the output should look like:
+```
+MOS score of the test speech (assuming NMR is clean) = 2.003323554992676
 ```
 
 Some GPU's are non-deterministic, and so the results could vary slightly in the lsb.
@@ -54,14 +71,28 @@ If you use this repository, please use the following to cite.
 
 ```
 @inproceedings{
-manocha2021noresqa,
+noresqa,
 title={{NORESQA}: A Framework for Speech Quality Assessment using Non-Matching References},
 author={Pranay Manocha and Buye Xu and Anurag Kumar},
 booktitle={Thirty-Fifth Conference on Neural Information Processing Systems},
 year={2021},
-url={https://openreview.net/forum?id=RwASmRpLp-}
+url={https://proceedings.neurips.cc/paper/2021/file/bc6d753857fe3dd4275dff707dedf329-Paper.pdf}
+}
+
+@inproceedings{
+noresqamos,
+title={Speech Quality Assessment through MOS using Non-Matching References},
+author={Pranay Manocha and Anurag Kumar},
+booktitle={Interspeech},
+year={2022},
+url={https://arxiv.org/abs/2206.12285}
 }
 ```
 
 ### License
 The majority of NORESQA is licensed under CC-BY-NC, however portions of the project are available under separate license terms: Librosa is licensed under the ISC license; Pytorch and Numpy are licensed under the BSD license; Scipy and Scikit-learn is licensed under the BSD-3; Libsndfile is licensed under GNU LGPL; Pyyaml is licensed under MIT License.
+
+### References
+[1] Le Roux, J., Wisdom, S., Erdogan, H., & Hershey, J. R. (2019, May). SDR–half-baked or well done?. In ICASSP 2019-2019 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP) (pp. 626-630). IEEE.
+
+[2] Loizou, Philipos C. "Speech quality assessment." In Multimedia analysis, processing and communications, pp. 623-654. Springer, Berlin, Heidelberg, 2011.
--- model.py
+++ model.py
@@ -9,13 +9,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils import weight_norm
-
+import numpy as np
+from librosa.filters import mel as librosa_mel_fn
+from torch.nn import Parameter
+from functools import wraps
+import fairseq
+from fairseq import tasks
+import pickle
 
 class model_dimred(nn.Module):
-    
+
     def __init__(self, in_channel=64, conv1x1=16, reduce3x3=24, conv3x3=32, reduce5x5=16, conv5x5=8, pool_proj=8, pool=2):
         super(model_dimred, self).__init__()
-        
+
         self.modules1 = nn.ModuleList()
         self.modules1.append(nn.Conv2d(in_channel, conv1x1, 1, (1,1), 0))
         self.modules1.append(nn.Conv2d(in_channel, reduce3x3, 1, 1, 0))
@@ -25,9 +31,9 @@ class model_dimred(nn.Module):
         self.modules1.append(nn.MaxPool2d((3,3),stride=(1,1),padding=(1,1)))
         self.modules1.append(nn.Conv2d(in_channel, pool_proj, 1, 1, 0))
         self.modules1.append(nn.MaxPool2d((1,pool)))
-        
+
     def forward(self, x):
-        
+
         a = F.relu(self.modules1[0](x))
         b = F.relu(self.modules1[2]((F.relu(self.modules1[1](x)))))
         c = F.relu(self.modules1[4]((F.relu(self.modules1[3](x)))))
@@ -37,28 +43,28 @@ class model_dimred(nn.Module):
         x2 = F.relu(self.modules1[7](x1))
         return x2
 
-        
+
 class base_encoder(nn.Module):
     def __init__(self,dev=torch.device('cpu')):
         super(base_encoder, self).__init__()
         self.dev = dev
-        
+
         self.modelA = model_dimred(in_channel=2, pool=4)
         self.modelB = model_dimred(in_channel=64, pool=4)
         self.modelC = model_dimred(in_channel=64, pool=4)
         self.modelD = model_dimred(in_channel=64, pool=2)
-        
-        
+
+
     def forward(self,x):
         x = (self.modelD(self.modelC(self.modelB(self.modelA(x)))))
         return x
-    
+
 
 class which_clean(nn.Module):
     def __init__(self):
         super(which_clean, self).__init__()
         n_layers = 2
-        
+
         self.encoder = nn.ModuleList()
         self.ebatch = nn.ModuleList()
         self.dp = nn.ModuleList()
@@ -73,10 +79,10 @@ class which_clean(nn.Module):
         self.encoder.append(nn.Conv1d(8,2,filter_size,padding=filter_size//2))
         self.ebatch.append(nn.BatchNorm1d(2))
         self.dp.append(nn.Dropout(p=dp_num))
-        
-    
+
+
     def forward(self,x):
-        
+
         for i in range(3):
             x = self.encoder[i](x)
             x = self.ebatch[i](x)
@@ -84,12 +90,12 @@ class which_clean(nn.Module):
                 x = F.leaky_relu(x,0.1)
             x = self.dp[i](x)
         return x
-    
+
 class how_snr(nn.Module):
     def __init__(self,dim_emb=32, output=50):
         super(how_snr, self).__init__()
         n_layers = 2
-        
+
         self.encoder = nn.ModuleList()
         self.ebatch = nn.ModuleList()
         self.dp = nn.ModuleList()
@@ -104,7 +110,7 @@ class how_snr(nn.Module):
         self.encoder.append(nn.Conv1d(32,output,filter_size,padding=filter_size//2))
         self.ebatch.append(nn.BatchNorm1d(output))
         self.dp.append(nn.Dropout(p=dp_num))
-        
+
     def forward(self,x):
 
         for i in range(3):
@@ -114,12 +120,12 @@ class how_snr(nn.Module):
                 x = F.leaky_relu(x,0.1)
             x = self.dp[i](x)
         return x
-    
+
 class how_snr_snr(nn.Module):
     def __init__(self,dim_emb=32, output=50):
         super(how_snr_snr, self).__init__()
         n_layers = 2
-        
+
         self.encoder = nn.ModuleList()
         self.ebatch = nn.ModuleList()
         self.dp = nn.ModuleList()
@@ -134,9 +140,9 @@ class how_snr_snr(nn.Module):
         self.encoder.append(nn.Conv1d(32,output,filter_size,padding=filter_size//2))
         self.ebatch.append(nn.BatchNorm1d(output))
         self.dp.append(nn.Dropout(p=dp_num))
-        
+
     def forward(self,x):
-        
+
         for i in range(3):
             x = self.encoder[i](x)
             x = self.ebatch[i](x)
@@ -144,42 +150,77 @@ class how_snr_snr(nn.Module):
                 x = F.leaky_relu(x,0.1)
             x = self.dp[i](x)
         return x
-    
 
 class NORESQA(nn.Module):
-    
-    def __init__(self,dev=torch.device('cpu'),minit=1, output=20,output2=16):
+
+    def __init__(self,dev=torch.device('cpu'), minit=1, output=20,output2=16, metric_type=0, config_path='models/wav2vec_small.pt'):
         super(NORESQA, self).__init__()
-        self.base_encoder = base_encoder()
-        
-        self.base_encoder_2 = TemporalConvNet(num_inputs=128,num_channels=[32,64,128,64],kernel_size=3)
-        
-        self.which_clean = which_clean()
-        self.how_snr_sdr = how_snr(output=output)
-        self.how_snr_snr = how_snr_snr(output=output2)
-        if minit==1:
-            self.base_encoder.apply(weights_init)
-            self.which_clean.apply(weights_init)
-            self.how_snr_sdr.apply(weights_init)
-            self.how_snr_snr.apply(weights_init)
-        self.CE = nn.CrossEntropyLoss(reduction='mean')
-            
-    
-    def forward(self,x1,x2):
-        
-        x1 = self.base_encoder.forward(x1)
-        x2 = self.base_encoder.forward(x2)
-        x1=self.base_encoder_2(x1)
-        x2=self.base_encoder_2(x2)
-        
-        concat = torch.cat((x1,x2), 1)
-        
-        which_closer = self.which_clean.forward(concat)
-        sdr_diff = self.how_snr_sdr.forward(concat)
-        snr_diff = self.how_snr_snr.forward(concat)
-        
-        return which_closer,sdr_diff,snr_diff    
-    
+
+        self.metric_type = metric_type
+        if metric_type==0:
+            self.base_encoder = base_encoder()
+            self.base_encoder_2 = TemporalConvNet(num_inputs=128,num_channels=[32,64,128,64],kernel_size=3)
+
+            self.which_clean = which_clean()
+            self.how_snr_sdr = how_snr(output=output)
+            self.how_snr_snr = how_snr_snr(output=output2)
+            if minit == 1:
+                self.base_encoder.apply(weights_init)
+                self.which_clean.apply(weights_init)
+                self.how_snr_sdr.apply(weights_init)
+                self.how_snr_snr.apply(weights_init)
+            self.CE = nn.CrossEntropyLoss(reduction='mean')
+
+        elif metric_type == 1:
+
+            SSL_OUT_DIM=768
+            ssl_model, _, _ = fairseq.checkpoint_utils.load_model_ensemble_and_task([config_path])
+
+            ssl_model = ssl_model[0]
+
+            ssl_model.remove_pretraining_modules()
+            self.main_model = MosPredictor(ssl_model, SSL_OUT_DIM)
+            self.linear_layer = nn.Linear(SSL_OUT_DIM, 32)
+
+            self.quantification = PoolAtt(d_input=64,output_size=5)
+            self.preference = PoolAtt(d_input=64,output_size=2)
+
+
+    def forward(self, x1, x2 = None):
+
+        if self.metric_type == 0:
+            x1 = self.base_encoder.forward(x1)
+            x2 = self.base_encoder.forward(x2)
+            x1=self.base_encoder_2(x1)
+            x2=self.base_encoder_2(x2)
+
+            concat = torch.cat((x1,x2), 1)
+
+            which_closer = self.which_clean.forward(concat)
+            sdr_diff = self.how_snr_sdr.forward(concat)
+            snr_diff = self.how_snr_snr.forward(concat)
+
+            return which_closer, sdr_diff, snr_diff
+
+        elif self.metric_type == 1:
+
+            x1 = self.linear_layer(self.main_model(x1)).permute(0,2,1)
+            y1 = self.linear_layer(self.main_model(x2)).permute(0,2,1)
+            concat = torch.cat((x1,y1), 1)
+
+            n_wins = concat.shape[2]
+            B = [n_wins for n in range(concat.shape[0])]
+            n_wins_tensor = torch.from_numpy(np.asarray(B)).to(concat.device)
+
+            pref = self.preference(concat.permute(0,2,1),n_wins_tensor)
+            quantf = self.quantification(concat.permute(0,2,1),n_wins_tensor)
+
+            att = F.softmax(quantf, dim=1)
+            B = torch.linspace(0, 4, steps=5).to(concat.device)
+            C = (att*B).sum(axis=1)
+            return C
+
+
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1 or classname.find('BatchNorm') != -1 or classname.find('Linear') != -1:
@@ -188,7 +229,7 @@ def weights_init(m):
             torch.nn.init.constant_(m.bias, 0.01)
         except:
             pass
-    
+
 
 
 class TemporalBlock(nn.Module):
@@ -196,7 +237,7 @@ class TemporalBlock(nn.Module):
         super(TemporalBlock, self).__init__()
         self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
                                            stride=stride, padding=dilation, dilation=dilation))
-        
+
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
 
@@ -238,7 +279,46 @@ class TemporalConvNet(nn.Module):
         self.network = nn.Sequential(*layers)
 
     def forward(self, x1):
-        
+
         x1 = x1.reshape(x1.shape[0],-1,x1.shape[2])
         x = self.network(x1)
+        return x
+
+
+class MosPredictor(nn.Module):
+    def __init__(self, ssl_model, ssl_out_dim):
+        super(MosPredictor, self).__init__()
+        self.ssl_model = ssl_model
+        self.ssl_features = ssl_out_dim
+
+    def forward(self, wav):
+        wav = wav.squeeze(1)  ## [batches, audio_len]
+        res = self.ssl_model(wav, mask=False, features_only=True)
+        x = res['x']
+
+        return x
+
+
+class PoolAtt(torch.nn.Module):
+    '''
+    PoolAtt: Attention-Pooling module.
+    '''
+    def __init__(self, d_input, output_size):
+        super().__init__()
+
+        self.linear1 = nn.Linear(d_input, 1)
+        self.linear2 = nn.Linear(d_input, output_size)
+
+    def forward(self, x, n_wins):
+
+        att = self.linear1(x) # B X T X C
+
+        att = att.transpose(2,1) # B X 1 X T
+        mask = torch.arange(att.shape[2])[None, :] < n_wins[:, None].to('cpu').to(torch.long)
+        att[~mask.unsqueeze(1)] = float("-Inf")
+        att = F.softmax(att, dim=2)
+        x = torch.bmm(att, x)
+        x = x.squeeze(1)
+        x = self.linear2(x)
+
         return x
